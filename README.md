389eacde81ef6ab9461ec9628d62d1e67ce1d179
stephan du Plooy <sdpphotography1@gmail.com>
2021-03-26 08:27:04 -0400

    Displayed Time to Screen and started writing Script to save user inputs + deleted old files

--- README.md
+++
@@ -1,3 +0,0 @@
-# Steph-s-Amazing-Day-Scheduler
-
-The goal of this day planner is to create an organized day planner, where an individual can organize, drag, and drop their tasks for the day and stay organized.
--- script.js
+++ script.js
@@ -1,7 +1,56 @@
-var displayTimeEL = document.querySelector(".lead");
+var timeDisplayEL = $("#time-display");
+var timeBlock = $(".time-block")
+var timeArea = document.querySelector(".time-block > textarea")
 
-//display time at top of the screen
-function displayTime() {
-    var currentTime = moment().format('MMM, DD, YYY');
-    displayTimeEL.textContent(currentTime);
+console.log("hi"); 
+
+
+//display time
+$("#today").text(moment().format('MMM Do, YYYY; h:mm a'));
+
+var currentHour = new Date();
+var hour = currentHour.getHours();
+if (hour >= 0 &&)
+
+// //color gets changed depending on time, this accesses the time 
+// var date = new Date();
+// var hour = date.getHours();
+// function presentTime() {
+//     // time might need to point to your time blocks - (done)
+//     var time = $("#t")
+//     var myHour = timeBlock.attr('id').split('t')[0]
+//     console.log(myHour)
+//     for (time = 9; time < 18; time ++) {
+//         // for each of your time block
+//         // I want to see if the time is pas t, present etc
+//         // use the id in each time-block
+
+//         //different classes get applied to the time blocks depending on the time
+//         if(time === hour) {
+//             timeArea.addClass("present") 
+//         } else if (time < hour) {
+//             timeArea.addClass("past")
+//         } else {
+//             timeArea.addClass("future")
+//         }
+
+//         //gets time out of local storage so it can display on screen
+//         $("#" + time).val(localStorage.getItem("" + time));
+//     }
+// }
+
+presentTime()
+
+//input gets saved to the list when the save button is pressed
+
+function userInput(event) {
+    var taskInput = $(this).siblings("textarea").val().trim();
+    var hoursV = $(this).siblings("textarea").attr("id");
+
+    localStorage.setItem(hoursV, taskInput);
 }
+
+$(".saveBtn").on("click", userInput)
+
+
+
